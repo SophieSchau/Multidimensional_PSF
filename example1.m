ee737e53502026e54f152615f5740afcf8e2a029
Sophie <sophie.schauman@dtc.ox.ac.uk>
2020-03-13 11:38:02 +0000

    fixed typos

--- example1.m
+++ example1.m
@@ -1,15 +1,15 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%   Example for how to use the m_PSF    %
-%                                       %
-%   Sophie Schauman 2020                %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   Simple example for how to use the m_PSF    %
+%                                              %
+%   Sophie Schauman 2020                       %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 clear
 close all
 
 %% 1. Define k-space sampling pattern
 %   We will use golden angle radial sampling within each encoding. 4
-%   encodings with 100 spokes each.
+%   encodings with 10 spokes each.
 %
 %   Option 1: Same sampling pattern for each encoding
 %   Option 2: Sample pattern rotated by 45 degrees between each encoding
@@ -21,7 +21,7 @@ k2 = create_radial_traj(4,10,'ga','uniform'); % option 2
 figure(1)
 subplot(1,2,1)
 for enc = 1:4
-scatter(k1(:,1,enc,1), k1(:,1,enc,2))
+scatter(k1(:,1,enc,1), k1(:,1,enc,2),50/enc)
 hold on
 end
 legend('encoding 1', 'encoding 2','encoding 3','encoding 4')
@@ -30,14 +30,14 @@ title('same spokes every encoding')
 
 subplot(1,2,2)
 for enc = 1:4
-scatter(k2(:,1,enc,1), k2(:,1,enc,2))
+scatter(k2(:,1,enc,1), k2(:,1,enc,2),50/enc)
 hold on
 end
 legend('encoding 1', 'encoding 2','encoding 3','encoding 4')
 axis equal
 title('different spokes each encoding')
 %% 2. Choose encoding matrix
-%   We will use a 4x4 hadamerd encoding scheme
+%   We will use a 4x4 hadamard encoding scheme
 
 H = [-1 -1 -1  1;...
     -1  1  1  1;...
@@ -46,14 +46,16 @@ H = [-1 -1 -1  1;...
 
 %% 3. Generate multidimensional PSFs
 %   We use a 64x64 field of view, with 10 spokes per encoding and 
-%   approximately uniform sampling R = 64*pi*0.5/10 = approx. 10.
+%   approximately uniform sampling R = 64*pi*0.5/10 = approx. 10. We assume
+%   that every component has equal energy.
 
 pause(1)
 figure(2)
-m_psf(k1, H, [64 64], [1 1 1 1]);
+mpsf1 = m_psf(k1, H, [64 64]);
 title('m-PSF for same spokes every encoding')
 
 pause(1)
 figure(3)
-m_psf(k2, H, [64 64], [1 1 1 1]);
+mpsf2 = m_psf(k2, H, [64 64]);
 title('m-PSF for different spokes each encoding')
+ 
