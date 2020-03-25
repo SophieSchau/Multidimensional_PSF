e9fbb95eaa043986d73365ad048d318b352785e1
Sophie <sophie.schauman@dtc.ox.ac.uk>
2020-03-25 17:02:55 +0000

    bugs fixed and rng seeding method updated to always give unique seed for each v essel component and augmentation

--- experiment_recon_decode_interactions.m
+++ experiment_recon_decode_interactions.m
@@ -14,8 +14,8 @@ phantomname = 'phantom64x64';
 N_augmentations = 10;
 N_noise_instances = 10;
 N_total_spokes = 16;
-full_bg_supp = true;
-lambda = 1;
+full_bg_supp = false;
+lambda = 5;
 
 load(['data/' phantomname], 'GT');
 if full_bg_supp
@@ -25,38 +25,34 @@ end
 %% 2a. Setup forward model - Same spokes each encoding + 4x4 encoding
 
 % trajectory
-k_a = create_radial_traj(4,N_total_spokes/4,'ga','same'); 
+k_same = create_radial_traj(4,N_total_spokes/4,'ga','same'); 
 
 % encoding matrix
-H_a = [-1 -1 -1  1;...
+H_4 = [-1 -1 -1  1;...
     -1  1  1  1;...
     1 -1  1  1;...
     1  1 -1  1];
 
 % acquisition operator
-E_a = xfm_NUFFT_LinearEncoding([64 64 1 1 size(H_a,2)],ones(64),[],k_a,H_a);
+E_same = xfm_NUFFT_LinearEncoding([64 64 1 1 size(H_4,2)],ones(64),[],k_same,H_4);
 
 %% 2b. Setup forward model - Varying spokes each encoding + 4x4 encoding
 
 % trajectory
-k_b = create_radial_traj(4,N_total_spokes/4,'ga','uniform'); 
+k_vary = create_radial_traj(4,N_total_spokes/4,'ga','uniform'); 
 
-% encoding matrix
-H_b = [-1 -1 -1  1;...
-    -1  1  1  1;...
-    1 -1  1  1;...
-    1  1 -1  1];
+% encoding matrix = H_4
 
 % acquisition operator
-E_b = xfm_NUFFT_LinearEncoding([64 64 1 1 size(H_b,2)],ones(64),[],k_b,H_b);
+E_vary = xfm_NUFFT_LinearEncoding([64 64 1 1 size(H_4,2)],ones(64),[],k_vary,H_4);
 
 %% 2c. Setup forward model - Varying spokes every other encoding + 8x4 encoding
 
 % trajectory
-k_c = create_radial_traj(8,N_total_spokes/8,'ga','same_uniform_alt'); 
+k_alt = create_radial_traj(8,N_total_spokes/8,'ga','same_uniform_alt'); 
 
 % encoding matrix
-H_c = [ -1 -1 -1  1;...
+H_8 = [ -1 -1 -1  1;...
         1  1  1  1;...
         -1  1  1  1;...
         1 -1 -1  1;...
@@ -66,63 +62,131 @@ H_c = [ -1 -1 -1  1;...
         -1 -1  1  1];
 
 % acquisition operator
-E_c = xfm_NUFFT_LinearEncoding([64 64 1 1 size(H_c,2)],ones(64),[],k_c,H_c);
+E_alt = xfm_NUFFT_LinearEncoding([64 64 1 1 size(H_8,2)],ones(64),[],k_alt,H_8);
 
 %% 3. Simulate k-space data and reconstruct
 
 for aug = 1:N_augmentations
     for vc = 1: size(GT,5)
-        GT(:,:,:,:,vc) = perturb_image(GT(:,:,:,:,vc),1,5,0.5 ,aug+vc);
+        rng_seed = 2^aug*3^vc;
+        GT_aug(:,:,:,:,vc) = perturb_image(GT(:,:,:,:,vc),1,5,0.5 ,rng_seed);
     end
     for n = 1:N_noise_instances
-        savefilename = ['data/recons/' phantomname '_recons_spokes' num2str(N_total_spokes) '_lambda' num2str(lambda) '_pertubation' num2str(aug) '_noise' num2str(n)];
+        savefilename = ['data/recons/' phantomname '_recons_spokes' num2str(N_total_spokes) '_lambda' num2str(lambda) '_pertubation' num2str(aug) '_noise' num2str(n) '_bgsupp' num2str(full_bg_supp) '.mat'];
         if ~exist(savefilename,'file')
             rng(n);
-            noise = (10/sqrt(2))*(randn(prod(E_a.dsize),1)+1i*randn(prod(E_a.dsize),1));
+            noise = (10/sqrt(2))*(randn(prod(E_same.dsize),1)+1i*randn(prod(E_same.dsize),1));
 
-            kdata_a = E_a*GT + reshape(noise, E_a.dsize);
-            kdata_b = E_b*GT + reshape(noise, E_b.dsize);
-            kdata_c = E_c*GT + reshape(noise, E_c.dsize);
+            kdata_same = E_same*GT_aug + reshape(noise, E_same.dsize);
+            kdata_vary = E_vary*GT_aug + reshape(noise, E_vary.dsize);
+            kdata_alt = E_alt*GT_aug + reshape(noise, E_alt.dsize);
 
             % Decode + recon (only possible for same spokes every encoding)
-            kdata_a_decoded = reshape((H_a'*reshape(kdata_a, [],size(H_a,1))')',[size(kdata_a,1),size(kdata_a,2),size(H_a,2)]);
-            H1 = eye(size(H_a,1));
-            E1 = xfm_NUFFT_LinearEncoding([64 64 1 1 size(H1,2)],ones(64),[],k_a,H1);
+            kdata_same_decoded = reshape((H_4'*reshape(kdata_same, [],size(H_4,1))')',[size(kdata_same,1),size(kdata_same,2),size(H_4,2)]);
+            H1 = eye(size(H_4,1));
+            E1 = xfm_NUFFT_LinearEncoding([64 64 1 1 size(H1,2)],ones(64),[],k_same,H1);
 
-            image_a_decode_recon = abs(fista_general(kdata_a_decoded, E1 , 1,...
+            image_same_decode_recon = abs(fista_general(kdata_same_decoded, E1 , 1,...
                 lambda, 300, 0.01, 'showProgress', false));
 
             % Recon + decode
-            H1 = eye(size(H_a,2));
-            H2 = eye(size(H_c,1));
-            E1_ab = xfm_NUFFT_LinearEncoding([64 64 1 1 size(H1,2)],ones(64),[],k_a,H1);
-            E1_c = xfm_NUFFT_LinearEncoding([64 64 1 1 size(H2,2)],ones(64),[],k_c,H2);
+            H1 = eye(size(H_4,1));
+            H2 = eye(size(H_8,1));
+            E1_same = xfm_NUFFT_LinearEncoding([64 64 1 1 size(H1,2)],ones(64),[],k_same,H1);
+            E1_vary = xfm_NUFFT_LinearEncoding([64 64 1 1 size(H1,2)],ones(64),[],k_vary,H1);
+            E1_alt = xfm_NUFFT_LinearEncoding([64 64 1 1 size(H2,2)],ones(64),[],k_alt,H2);
 
-            image_a_recon = abs(fista_general(kdata_a, E1_ab , 1,...
-                lambda, 300, 0.01, 'showProgress', false));
-            image_b_recon = abs(fista_general(kdata_b, E1_ab , 1,...
-                lambda, 300, 0.01, 'showProgress', false));
-            image_c_recon = abs(fista_general(kdata_c, E1_c , 1,...
-                lambda, 300, 0.01, 'showProgress', false));
+            image_same_recon = fista_general(kdata_same, E1_same , 1,...
+                lambda, 300, 0.01, 'showProgress', false);
+            image_vary_recon = fista_general(kdata_vary, E1_vary , 1,...
+                lambda, 300, 0.01, 'showProgress', false);
+            image_alt_recon = fista_general(kdata_alt, E1_alt , 1,...
+                lambda, 300, 0.01, 'showProgress', false);
 
 
-            image_a_recon_decode = reshape((H_a'*reshape(image_a_recon, [],size(H_a,1))')',[size(image_a_recon,1),size(image_a_recon,2),size(image_a_recon,3),size(image_a_recon,4),size(H_a,2)]);
-            image_b_recon_decode = reshape((H_b'*reshape(image_b_recon, [],size(H_b,1))')',[size(image_b_recon,1),size(image_b_recon,2),size(image_b_recon,3),size(image_b_recon,4),size(H_b,2)]);
-            image_c_recon_decode = reshape((H_c'*reshape(image_c_recon, [],size(H_c,1))')',[size(image_c_recon,1),size(image_c_recon,2),size(image_c_recon,3),size(image_c_recon,4),size(H_c,2)]);
+            image_same_recon_decode = abs(reshape((H_4'*reshape(image_same_recon, [],size(H_4,1))')',size(GT)));
+            image_vary_recon_decode = abs(reshape((H_4'*reshape(image_vary_recon, [],size(H_4,1))')',size(GT)));
+            image_alt_recon_decode = abs(reshape((H_8'*reshape(image_alt_recon, [],size(H_8,1))')',size(GT)));
 
 
             % joint recon and decoding
-            image_a_joint = abs(fista_general(kdata_a, E_a , 1,...
+            image_same_joint = abs(fista_general(kdata_same, E_same , 1,...
                 lambda, 300, 0.01, 'showProgress', false));
-            image_b_joint = abs(fista_general(kdata_b, E_b , 1,...
+            image_vary_joint = abs(fista_general(kdata_vary, E_vary , 1,...
                 lambda, 300, 0.01, 'showProgress', false));
-            image_c_joint = abs(fista_general(kdata_c, E_c , 1,...
+            image_alt_joint = abs(fista_general(kdata_alt, E_alt , 1,...
                 lambda, 300, 0.01, 'showProgress', false));
 
             save(savefilename,...
-                'image_a_decode_recon','image_a_joint','image_a_recon_decode', 'image_b_joint', 'image_b_recon_decode', 'image_c_joint', 'image_c_recon_decode')
+                'image_same_decode_recon','image_same_joint','image_same_recon_decode', 'image_vary_joint', 'image_vary_recon_decode', 'image_alt_joint', 'image_alt_recon_decode')
         end
     end
 
 end
 
+%% 4. Compare with ground truth
+
+for aug = 1:N_augmentations
+    for vc = 1: size(GT,5)
+        rng_seed = 2^aug*3^vc;
+        GT_aug(:,:,:,:,vc) = perturb_image(GT(:,:,:,:,vc),1,5,0.5 ,rng_seed);
+    end
+    for n = 1:N_noise_instances
+        loadfilename = ['data/recons/' phantomname '_recons_spokes' num2str(N_total_spokes) '_lambda' num2str(lambda) '_pertubation' num2str(aug) '_noise' num2str(n) '_bgsupp' num2str(full_bg_supp) '.mat'];
+        load(loadfilename);
+        
+        [~, gof] =fit(reshape(GT_aug(:,:,:,:,1:end-1),[],1),reshape(image_same_joint(:,:,:,:,1:end-1),[],1),'poly1');
+        rsquare_same_joint(aug,n) = gof.rsquare;
+        rmserror_same_joint(aug,n) = gof.rmse;
+        
+        [~, gof] =fit(reshape(GT_aug(:,:,:,:,1:end-1),[],1),reshape(image_same_recon_decode(:,:,:,:,1:end-1),[],1),'poly1');
+        rsquare_same_recon_decode(aug,n) = gof.rsquare;
+        rmserror_same_recon_decode(aug,n) = gof.rmse;
+        
+        [~, gof] =fit(reshape(GT_aug(:,:,:,:,1:end-1),[],1),reshape(image_same_decode_recon(:,:,:,:,1:end-1),[],1),'poly1');
+        rsquare_same_decode_recon(aug,n) = gof.rsquare;
+        rmserror_same_decode_recon(aug,n) = gof.rmse;
+        
+        
+        
+        
+        [~, gof] =fit(reshape(GT_aug(:,:,:,:,1:end-1),[],1),reshape(image_vary_joint(:,:,:,:,1:end-1),[],1),'poly1');
+        rsquare_vary_joint(aug,n) = gof.rsquare;
+        rmserror_vary_joint(aug,n) = gof.rmse;
+        
+        [~, gof] =fit(reshape(GT_aug(:,:,:,:,1:end-1),[],1),reshape(image_vary_recon_decode(:,:,:,:,1:end-1),[],1),'poly1');
+        rsquare_vary_recon_decode(aug,n) = gof.rsquare;
+        rmserror_vary_recon_decode(aug,n) = gof.rmse;
+
+        
+        
+        
+        
+        [~, gof] =fit(reshape(GT_aug(:,:,:,:,1:end-1),[],1),reshape(image_alt_joint(:,:,:,:,1:end-1),[],1),'poly1');
+        rsquare_alt_joint(aug,n) = gof.rsquare;
+        rmserror_alt_joint(aug,n) = gof.rmse;
+        
+        [~, gof] =fit(reshape(GT_aug(:,:,:,:,1:end-1),[],1),reshape(image_alt_recon_decode(:,:,:,:,1:end-1),[],1),'poly1');
+        rsquare_alt_recon_decode(aug,n) = gof.rsquare;
+        rmserror_alt_recon_decode(aug,n) = gof.rmse;
+        
+      
+    end
+
+end
+
+%% 5. Vizualise result
+
+rsquare_same_decode_recon_mean = mean(rsquare_same_decode_recon(:));
+rsquare_same_recon_decode_mean = mean(rsquare_same_recon_decode(:));
+rsquare_same_joint_mean = mean(rsquare_same_joint(:));
+
+rsquare_vary_recon_decode_mean = mean(rsquare_vary_recon_decode(:));
+rsquare_vary_joint_mean = mean(rsquare_vary_joint(:));
+
+rsquare_alt_recon_decode_mean = mean(rsquare_alt_recon_decode(:));
+rsquare_alt_joint_mean = mean(rsquare_alt_joint(:));
+
+barh([rsquare_same_decode_recon_mean 0 0;...
+      rsquare_same_recon_decode_mean, rsquare_vary_recon_decode_mean, rsquare_alt_recon_decode_mean;...
+      rsquare_same_joint_mean, rsquare_vary_joint_mean, rsquare_alt_joint_mean]')
